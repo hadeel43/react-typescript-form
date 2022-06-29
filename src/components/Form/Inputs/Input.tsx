4d219047114468850289c6fb5dca4597d1b0eba5
Hadil Alabrash <hadeelalabrash43@gmail.com>
2022-06-29 18:05:25 +0200

    Update Input.tsx

--- src/components/Form/Inputs/Input.tsx
+++ src/components/Form/Inputs/Input.tsx
@@ -1,46 +1,41 @@
 import classNames from "classnames";
+
 interface InputProps {
-    label?: string,
-    subLabel?: string,
-    required?: boolean,
-    render: any
-    className?: string,
-    type?: string
+  label?: string;
+  subLabel?: string;
+  required?: boolean;
+  render: any;
+  className?: string;
 }
 
 const Input = ({
-    label,
-    subLabel,
-    required = false,
-    className = "",
-    type,
-    render }: InputProps) => {
-    return (
-        <div
-            className={classNames({
+  label,
+  subLabel,
+  required = false,
+  className = "",
+  render,
+}: InputProps) => (
+  <div
+    className={classNames({
+      [className]: true,
+    })}
+  >
+    {label && (
+      <label className="mr-1 mb-1 font-light">
+        {label}
+        {required && (
+          <>
+            <span className="text-red-300">*</span>
+          </>
+        )}
+      </label>
+    )}
 
-                [className]: true,
-            })}
-        >
-            {label && (
-                <label className="font-light mb-1 mr-1">
-                    {label}
-                    {required && (
-                        <>
-                            <span className="text-brand-red">*</span>
-                        </>
-                    )}
-                </label>
-            )}
-
-            {render}
-            {subLabel && (
-                <span className="text-sm text-gray-700 font-light py-2">
-                    {subLabel}
-                </span>
-            )}
-        </div>
-    );
-};
+    {render}
+    {subLabel && (
+      <span className="py-2 text-sm font-light text-gray-700">{subLabel}</span>
+    )}
+  </div>
+);
 
 export default Input;
